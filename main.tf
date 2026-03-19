f1b7d319925843728e5f7bf854ad8b5277e35ba4
unix-eye <pgbois@gmail.com>
2026-03-19 16:33:45 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,6 @@ terraform {
 }
 
 resource "google_compute_network" "example_vpc" {
-  name                    = "example-vpc"
+  name                    = "example-vpc-pb"
   auto_create_subnetworks = true 
 }
